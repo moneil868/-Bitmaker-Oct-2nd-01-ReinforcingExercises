97350637cdd6c9caa5a59ae183deac18c92a9f4a
Marlon O'Neil <hello@marlononeil.com>
2017-10-03 14:10:14 -0400

    Completed exercise

---
+++ exercise.js
@@ -0,0 +1,48 @@
+var project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+
+var count = 0
+project['steps'].forEach(function(step){
+    step.person = project.committee[count]
+    if( count < 2){
+      count++
+    }
+    else {
+      count = 0
+    }
+});
+
+console.log(project)
